2b6ea220f8f24b10f91d6803f5dec45833071786
RobArkesteijn <rarkesteijn@outlook.com>
2023-08-13 15:43:59 +0200

    Sets up the project further

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: [
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["prettier"],
+  rules: {},
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import svgLoader from 'vite-svg-loader';
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  app: {
+    head: {
+      title: 'Wijkgericht Samenwerken | Rijnmond Dokters',
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
+  css: ['@/assets/css/main.scss'],
+  vite: {
+    plugins: [svgLoader()],
+  },
+});
